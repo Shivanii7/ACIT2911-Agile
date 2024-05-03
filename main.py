b33dfee7c37c0b6ed9019862c40e3f412c763b89
nbVenti <nbVenti@gmail.com>
2024-05-03 11:17:50 -0700

    Logic

--- main.py
+++ main.py
@@ -13,10 +13,21 @@ db.init_app(app)
 @app.route("/")
 def homepage():
     data = db.session.execute(db.select(Expenses))
+    processed_data = []
     for i in data.scalars():
-        u = i.to_json()
-        print(u)
-    return render_template("base.html")
+        u = {
+            'id': i.id,
+            'name': i.name,
+            'amount': i.amount,
+            'date': i.date,
+            # 'description': i.description if i.description else 'N/A',
+            'balance': 'N/A'
+        }
+        processed_data.append(u)
+        # print(data)
+        
+    return render_template("base.html", transactions=processed_data)
+    # return render_template("base.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)

39ffcacc3583fab1f942a80cb064a10cf05ed1bf
michael flood <michael_flood_@hotmail.com>
2024-05-16 14:55:47 -0700

    darkmode properly changes colours on expense page.

--- static/expense.js
+++ static/expense.js
@@ -1,1 +1,21 @@
+let darko = false;
+let buttontext = document.getElementById('dmtoggle');
 
+function textswap() {
+    var buttontext = document.getElementById('dmtoggle');
+    if (buttontext.innerText == 'Dimmer') {
+        buttontext.innerText = 'Brighter', darktime();
+    } else {
+        buttontext.innerText = 'Dimmer', darktime();
+    }
+}
+function darktime() {
+    console.log('darkness')
+    darko = !darko
+    if (darko) {
+        document.body.classList.add('dark')
+    } else {
+        document.body.classList.remove('dark')
+    }
+    }
+document.getElementById('dmtoggle').onclick = darktime
